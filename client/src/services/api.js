c82cd512890a8f959b7d370b9555d217d610b0c1
ronaksavsani181 <ronaksavsani181@gmail.com>
2026-04-14 13:33:26 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://thread-theory-backend-rfzw.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
